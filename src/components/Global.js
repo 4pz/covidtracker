08e07f78b92f8757dca7f63d65cb62b36fefa1ac
Simple2006 <vinayven01@gmail.com>
2021-04-03 22:30:10 -0700

    changes

--- src/components/Global.js
+++ src/components/Global.js
@@ -3,14 +3,24 @@ import axios from "axios";
 
 function Global() {
     const [data, setData] = useState(0)
+    const [data1, setData1] = useState(0)
+    const [data2, setData2] = useState(0)
+    const [data3, setData3] = useState(0)
+    const [data4, setData4] = useState(0)
+    const [data5, setData5] = useState(0)
 
     useEffect(() => {
 
         function getData() {
             axios.get('https://api.covid19api.com/summary')
                 .then(response => {
-                    setData(response.data.Global);
-                    console.log("Updated")
+                    setData(response.data.Global.NewConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    setData1(response.data.Global.TotalConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    setData2(response.data.Global.NewDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    setData3(response.data.Global.TotalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    setData4(response.data.Global.NewRecovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    setData5(response.data.Global.TotalRecovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                    console.log("Global Summary Updated")
                 })
                 .catch(console.error);
         }
@@ -25,43 +35,47 @@ function Global() {
 
     }, [])
 
+    /*function formatNumber(num) {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }*/
+
     return (
         <div className="summary">
             <h1>World Summary</h1>
             <div className="newconfirmed">
                 <h2>New Cases</h2>
                 <div className="box">
-                    <h3>{data.NewConfirmed}</h3>
+                    <h3>{data}</h3>
                 </div>
             </div>
             <div className="totalconfirmed">
                 <h2>Total Cases</h2>
                 <div className="box">
-                    <h3>{data.TotalConfirmed}</h3>
+                    <h3>{data1}</h3>
                 </div>
             </div>
             <div className="newdeaths">
                 <h2>New Deaths</h2>
                 <div className="box">
-                    <h3>{data.NewDeaths}</h3>
+                    <h3>{data2}</h3>
                 </div>
             </div>
             <div className="totaldeaths">
                 <h2>Total Deaths</h2>
                 <div className="box">
-                    <h3>{data.TotalDeaths}</h3>
+                    <h3>{data3}</h3>
                 </div>
             </div>
             <div className="newrecovered">
                 <h2>New Recovered</h2>
                 <div className="box">
-                    <h3>{data.NewRecovered}</h3>
+                    <h3>{data4}</h3>
                 </div>
             </div>
             <div className="totalrecovered">
                 <h2>Total Recovered</h2>
                 <div className="box" style={{marginBottom: "20px"}}>
-                    <h3>{data.TotalRecovered}</h3>
+                    <h3>{data5}</h3>
                 </div>
             </div>
         </div>
--- src/components/TopCountries.js
+++ src/components/TopCountries.js
@@ -1,31 +1,88 @@
-import React from "react"
-//import axios from "axios";
+import React, {useEffect, useState} from "react"
+import axios from "axios";
 import america from "../img/usflag.png"
 import brazil from "../img/brazil.png"
 import india from "../img/india.ico"
+import france from "../img/france.png"
+import russia from "../img/russia.png"
 
 function TopCountries() {
+    const [TotalConfirmed, setTotalConfirmed] = useState(0)
+    const [TotalDeaths, setTotalDeaths] = useState(0)
+    const [TotalConfirmed1, setTotalConfirmed1] = useState(0)
+    const [TotalDeaths1, setTotalDeaths1] = useState(0)
+    const [TotalConfirmed2, setTotalConfirmed2] = useState(0)
+    const [TotalDeaths2, setTotalDeaths2] = useState(0)
+    const [TotalConfirmed3, setTotalConfirmed3] = useState(0)
+    const [TotalDeaths3, setTotalDeaths3] = useState(0)
+    const [TotalConfirmed4, setTotalConfirmed4] = useState(0)
+    const [TotalDeaths4, setTotalDeaths4] = useState(0)
+
+    useEffect(() => {
+
+        function getData() {
+            axios.get('https://api.covid19api.com/summary')
+                .then(response => {
+                    //setData(response.data.Countries);
+                    //setData(response.data["Countries"][181])
+                    setTotalConfirmed(response.data.Countries[181].TotalConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    setTotalDeaths(response.data.Countries[181].TotalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    setTotalConfirmed1(response.data.Countries[23].TotalConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    setTotalDeaths1(response.data.Countries[23].TotalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    setTotalConfirmed2(response.data.Countries[76].TotalConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    setTotalDeaths2(response.data.Countries[76].TotalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    setTotalConfirmed3(response.data.Countries[59].TotalConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    setTotalDeaths3(response.data.Countries[59].TotalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    setTotalConfirmed4(response.data.Countries[140].TotalConfirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    setTotalDeaths4(response.data.Countries[140].TotalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                    console.log("Top Countries Updated")
+                })
+                .catch(console.error);
+        }
+
+        getData()
+
+        const intervalId = setInterval(() => {
+            getData()
+        }, 60 * 1000);
+
+        return () => clearInterval(intervalId);
+
+    }, [])
+
     return (
         <div className="TopCountries">
             <h1>Top Countries by Case</h1>
             <div className="countries">
                 <div className="America country-box">
-                    <img className="img" src={america} alt={"america"}></img>
-                    <h2>United States</h2>
-                    <h3>Cases<br></br>{1024}</h3>
-                    <h3>Deaths<br></br>{1024}</h3>
+                    <img className="img" src={america} alt={"america"}/>
+                    <h2>1. United States</h2>
+                    <h3>Cases<br/>{TotalConfirmed}</h3>
+                    <h3>Deaths<br/>{TotalDeaths}</h3>
                 </div>
                 <div className="Brazil country-box">
-                    <img className="img" src={brazil} alt={"brazil"}></img>
-                    <h2>Brazil</h2>
-                    <h3>Cases<br></br>{1024}</h3>
-                    <h3>Deaths<br></br>{1024}</h3>
+                    <img className="img" src={brazil} alt={"brazil"}/>
+                    <h2>2. Brazil</h2>
+                    <h3>Cases<br/>{TotalConfirmed1}</h3>
+                    <h3>Deaths<br/>{TotalDeaths1}</h3>
                 </div>
                 <div className="India country-box">
-                    <img className="img" src={india} alt={"india"}></img>
-                    <h2>India</h2>
-                    <h3>Cases<br></br>{1024}</h3>
-                    <h3>Deaths<br></br>{1024}</h3>
+                    <img className="img" src={india} alt={"india"}/>
+                    <h2>3. India</h2>
+                    <h3>Cases<br/>{TotalConfirmed2}</h3>
+                    <h3>Deaths<br/>{TotalDeaths2}</h3>
+                </div>
+                <div className="France country-box">
+                    <img className="img" src={france} alt={"india"}/>
+                    <h2>4. France</h2>
+                    <h3>Cases<br/>{TotalConfirmed3}</h3>
+                    <h3>Deaths<br/>{TotalDeaths3}</h3>
+                </div>
+                <div className="Russia country-box">
+                    <img className="img" src={russia} alt={"india"}/>
+                    <h2>5. Russia</h2>
+                    <h3>Cases<br/>{TotalConfirmed4}</h3>
+                    <h3>Deaths<br/>{TotalDeaths4}</h3>
                 </div>
             </div>
         </div>
